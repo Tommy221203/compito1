b542f49d570da6c9956576dc1ca93613e1fbfcdf
Tommy221203 <55913204+Tommy221203@users.noreply.github.com>
2019-10-06 22:30:44 +0200

    Aggiungere i file di progetto.

---
+++ CalcolaMetà(int n)/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CalcolaMetà_int_n_
+{
+    public class Calcola_metà
+    {
+        public static int Meta(int n)
+        {
+            int a = n % 2;
+            if(a>0)
+            {
+                throw new Exception("il risultato è un numero deimale");
+            }
+            return a;
+        }
+
+    }
+}
---
+++ soluzione/Program.cs
@@ -0,0 +1,18 @@
+﻿using CalcolaMetà_int_n_;
+using System;
+
+
+namespace soluzione
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci il numero");
+            int a = int.Parse(Console.ReadLine());
+            double risultato = Calcola_metà.Meta(a);
+            Console.WriteLine($"il risultato è {a}");
+            Console.ReadLine();
+        }
+    }
+}
